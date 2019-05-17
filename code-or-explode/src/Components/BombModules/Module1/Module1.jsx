eec6b605c2af7baa8e55f63ddeea45c0f85c2e7a
Jolt64 <jonathantelford64@gmail.com>
2019-05-17 16:35:59 -0600

    The Bomb frame is loading random module

--- code-or-explode/src/Components/BombModules/Module1/Module1.jsx
+++ code-or-explode/src/Components/BombModules/Module1/Module1.jsx
@@ -4,7 +4,7 @@ import './Module1.css';
 
 function BombModule1(props) {
 
-    const { strikeAdd } = props
+  const { strikeAdd, positionId } = props
 
   return (
     <div className='bomb-module-1'>
@@ -14,6 +14,7 @@ function BombModule1(props) {
             <div onClick={() => strikeAdd()}>Green wire</div>
             <div onClick={() => alert('Good Work :D')}>Green wire</div>
         </div>
+        <div>{positionId}</div>
         <p>S: 584268</p>
     </div>
   )
